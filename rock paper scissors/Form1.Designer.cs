82991bc1bf68d2da8eb6007dc5812e49151ce53a
BuildTools <udenanton@hotmail.com>
2018-03-16 08:55:34 +0100

    UI

--- rock paper scissors/Form1.Designer.cs
+++ rock paper scissors/Form1.Designer.cs
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbxAllResults = new System.Windows.Forms.TextBox();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.tbxResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxComputersChoice = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbxComputersChoice);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbxResult);
+            this.groupBox1.Controls.Add(this.btnScissors);
+            this.groupBox1.Controls.Add(this.btnPaper);
+            this.groupBox1.Controls.Add(this.btnRock);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 236);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "rock paper scissors";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbxAllResults);
+            this.groupBox2.Location = new System.Drawing.Point(421, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(151, 207);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(421, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // tbxAllResults
+            // 
+            this.tbxAllResults.Location = new System.Drawing.Point(7, 19);
+            this.tbxAllResults.Multiline = true;
+            this.tbxAllResults.Name = "tbxAllResults";
+            this.tbxAllResults.ReadOnly = true;
+            this.tbxAllResults.Size = new System.Drawing.Size(138, 182);
+            this.tbxAllResults.TabIndex = 0;
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(6, 19);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(125, 23);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(137, 19);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(125, 23);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Location = new System.Drawing.Point(268, 19);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(125, 23);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            // 
+            // tbxResult
+            // 
+            this.tbxResult.Location = new System.Drawing.Point(6, 210);
+            this.tbxResult.Name = "tbxResult";
+            this.tbxResult.ReadOnly = true;
+            this.tbxResult.Size = new System.Drawing.Size(390, 20);
+            this.tbxResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Computers choice:";
+            // 
+            // tbxComputersChoice
+            // 
+            this.tbxComputersChoice.Location = new System.Drawing.Point(104, 184);
+            this.tbxComputersChoice.Name = "tbxComputersChoice";
+            this.tbxComputersChoice.ReadOnly = true;
+            this.tbxComputersChoice.Size = new System.Drawing.Size(86, 20);
+            this.tbxComputersChoice.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Rock paper scissors";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxComputersChoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxResult;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbxAllResults;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
